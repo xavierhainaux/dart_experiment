d06f8d9a6807a504b326bb41327524653b2476c4
Xavier Hainaux <xavier.hainaux@gaming1.com>
2018-03-05 09:19:44 +0100

    Utiliser la méthode getContext3d
    La méthode getContext3d fait déjà le travail de checker le context `webgl` ou `experimental-webgl` et expose les options possibles de manière typée.

--- libraries/learn_webgl/web/webgl_course_01_intro/00_minimal/main.dart
+++ libraries/learn_webgl/web/webgl_course_01_intro/00_minimal/main.dart
@@ -5,7 +5,7 @@ void main() {
   CanvasElement canvas = new CanvasElement();
   document.body.children.add(canvas);
 
-  webgl.RenderingContext gl = canvas.getContext("experimental-webgl") as webgl.RenderingContext;
+  webgl.RenderingContext gl = canvas.getContext3d();
 
   gl.clearColor(1.0, 0.0, 0.0, 1.0);
   gl.clear(webgl.COLOR_BUFFER_BIT);
